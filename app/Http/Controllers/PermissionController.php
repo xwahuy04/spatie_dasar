8e9892a72fe220791b5c7faf001baeb519bf4806
wirasy <qywurwat@gmail.com>
2025-02-26 14:53:46 +0700

    first commit, create permission controller resource

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Permission;
+use Illuminate\Http\Request;
+
+class PermissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+     // This method will show permission page
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+     // This method will create permission page
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+     // This method will insert permission page
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Permission $rc)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+
+     // This method will edit permission page
+    public function edit(Permission $rc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+
+    // This method to update permission page
+    public function update(Request $request, Permission $rc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+
+     // This method will delete permission page
+    public function destroy(Permission $rc)
+    {
+        //
+    }
+}
